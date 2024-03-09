6cd6bf5280df947bc905aa560225c9811ec4874b
DohnalMichal <michal.dohnal7@gmail.com>
2024-03-09 12:51:27 +0100

    Justify sign in and sign up pages

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,7 +1,11 @@
 import { SignUp } from '@clerk/nextjs'
 
 const SignUpPage = () => {
-  return <SignUp afterSignInUrl="/new-user" redirectUrl="/new-user" />
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <SignUp afterSignInUrl="/new-user" redirectUrl="/new-user" />
+    </div>
+  )
 }
 
 export default SignUpPage

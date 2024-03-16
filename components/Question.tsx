c26a94331f353fec6e40d6e8ebb764fdd8e04876
DohnalMichal <michal.dohnal7@gmail.com>
2024-03-16 15:21:21 +0100

    Update question component to utilize askQuestion func

--- components/Question.tsx
+++ components/Question.tsx
@@ -1,17 +1,26 @@
 'use client'
 
 import { type ChangeEvent, type FormEvent, useState } from 'react'
+import { askQuestion } from '@/utils/api'
 
 const Question = () => {
   const [value, setValue] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [response, setResponse] = useState()
 
   const onChange = (event: ChangeEvent<HTMLInputElement>) => {
     event.preventDefault()
     setValue(event.target.value)
   }
 
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault()
+
+    setLoading(true)
+    const answer = await askQuestion(value)
+    setResponse(answer)
+    setLoading(false)
+    setValue('')
   }
 
   return (
@@ -19,15 +28,22 @@ const Question = () => {
       <form onSubmit={handleSubmit} className="flex gap-4">
         <input
           type="text"
+          disabled={loading}
           value={value}
           onChange={onChange}
           placeholder="Ask a question..."
           className="border border-black/10 px-4 py-2 text-lg rounded-lg"
         />
-        <button type="submit" className="bg-blue-400 px-4 py-2 rounded-lg">
+        <button
+          disabled={loading}
+          type="submit"
+          className="bg-blue-400 px-4 py-2 rounded-lg"
+        >
           Ask
         </button>
       </form>
+      {loading && <div>Loading...</div>}
+      {response && <div>{response}</div>}
     </div>
   )
 }

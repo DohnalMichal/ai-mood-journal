8cd631eee2d55604fa4454c2657cf4029036bcfc
DohnalMichal <michal.dohnal7@gmail.com>
2024-03-18 06:47:28 +0100

    Fix link

--- components/NavItem.tsx
+++ components/NavItem.tsx
@@ -16,12 +16,14 @@ const NavItem = ({ link }: Props) => {
   const isActiveLink = (link: string) => pathname === link
 
   return (
-    <li
-      className={`px-3 py-1 text-xl ${isActiveLink(link.href) ? 'bg-gray-100' : ''} hover:bg-gray-100  rounded-xl cursor-pointer flex items-center gap-4`}
-    >
-      {link.icon}
-      <Link href={link.href}>{link.label}</Link>
-    </li>
+    <Link href={link.href}>
+      <li
+        className={`px-3 py-1 text-xl ${isActiveLink(link.href) ? 'bg-gray-100' : ''} hover:bg-gray-100  rounded-xl cursor-pointer flex items-center gap-4`}
+      >
+        {link.icon}
+        {link.label}
+      </li>
+    </Link>
   )
 }
 

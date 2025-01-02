b28d64a1183b21f08d31da9aa40875e05d77d615
Michal Dohnal <michal.dohnal7@gmail.com>
2025-01-02 17:42:36 +0100

    Update Editor UI

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -5,9 +5,10 @@ import { useAutosave } from 'react-autosave'
 import { capitalize, toString } from 'lodash'
 import { updateEntry } from '@/utils/api'
 import { provideDefaults } from '@/utils/analysis'
-import type { JournalEntry } from '@/types'
 import { Textarea } from './ui/textarea'
 import { Label } from './ui/label'
+import { Circle } from 'lucide-react'
+import type { JournalEntry } from '@/types'
 
 const Editor = ({ entry }: { entry: JournalEntry }) => {
   const [value, setValue] = useState(entry.content)
@@ -54,7 +55,7 @@ const Editor = ({ entry }: { entry: JournalEntry }) => {
       </div>
       <div>
         <div className="flex items-center py-3 gap-2">
-          <div className="rounded-full w-3 h-3" style={{ background: color }} />
+          <Circle size={16} fill={color} className="flex-shrink-0" />
           <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
             Analysis of your journal entry
           </h3>
@@ -64,7 +65,7 @@ const Editor = ({ entry }: { entry: JournalEntry }) => {
           {analysisData.map((item) => (
             <li
               key={item.name}
-              className="px-2 py-2 flex flex-col items-start gap-1"
+              className="pr-2 py-2 flex flex-col items-start gap-1"
             >
               <span className="text-md font-semibold">{item.name}</span>
               <span>{capitalize(item.value)}</span>

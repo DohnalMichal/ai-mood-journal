dc47be32472572b36ddec5046d22d36e405e7dcd
Michal Dohnal <michal.dohnal7@gmail.com>
2025-01-03 12:08:52 +0100

    Update landing page

--- app/page.tsx
+++ app/page.tsx
@@ -8,19 +8,37 @@ export default async function Home() {
   const href = userId ? '/journal' : '/sign-up'
 
   return (
-    <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
-      <div className="w-full max-w-[800px] mx-auto">
-        <h1 className="text-6xl mb-4">
+    <div className="w-screen h-screen dark bg-background flex flex-col items-center text-foreground">
+      <header className="max-w-5xl p-8 w-full flex flex-row justify-between">
+        <span className="font-bold tracking-tight">AI Journal</span>
+        <nav>
+          <ul className="flex flex-row space-x-4">
+            <li>
+              <Button asChild>
+                <Link href="/sign-up">Sign up</Link>
+              </Button>
+            </li>
+            <li>
+              <Button asChild>
+                <Link href="/sign-in">Sign in</Link>
+              </Button>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <div className="w-full h-full max-w-3xl mx-auto flex flex-col justify-center">
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-6xl">
           Unlock Your Potential: Journaling, Supercharged by AI.
         </h1>
-        <p className="text-2xl text-white/60 mb-4">
+        <p className="text-2xl text-muted-foreground leading-7 mt-6">
           Cutting-edge web app powered by artificial intelligence. Seamlessly
           journal your daily emotions and experiences.
         </p>
         <div>
           <Button
             asChild
-            className="bg-blue-500 text-gray-50 hover:bg-blue-600 transition-all"
+            size="lg"
+            className="mt-10 bg-blue-500 text-gray-50 hover:bg-blue-600 transition-all text-lg"
           >
             <Link href={href}>Get started</Link>
           </Button>
